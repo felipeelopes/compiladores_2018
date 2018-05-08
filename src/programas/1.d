92ec10e31713d69064d98ec3d1b3025a83b9fbb6
Felipe Lopes <felipe_lopes@outlook.com>
2018-05-08 20:02:05 -0300

    Iniciando desenvolvimento do analisador lexico. Carregado primeira lista de TOKENS seguindo documentacao

---
+++ src/programas/1.d
@@ -0,0 +1,10 @@
+main()
+	var real a;
+	var real b;
+	scan(a);
+	scan(b);
+	var real s;
+	s = a + b;
+	print ("soma = ");
+	printLn(s);
+end
---
+++ src/programas/2.d
@@ -0,0 +1,12 @@
+main()
+	var int numero;
+	scan(numero);
+	while numero <> 0 do
+		if numero mod 2 == 0 then
+			print("par");
+		else
+			print("impar);
+		end-if
+		scan (numero);
+	loop
+end
